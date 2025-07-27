81efb65f55531fb275ba516ee54662f8c72b1702
davidseo88 <willyhandoko2022@gmail.com>
2025-07-27 08:21:53 +0700

    Add files via upload

---
+++ shortq_clone_complete/server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const ShortURL = require("./models/ShortURL");
+const { nanoid } = require("nanoid");
+const path = require("path");
+const app = express();
+
+mongoose.connect("mongodb://localhost:27017/shortq", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", async (req, res) => {
+  const shortURLs = await ShortURL.find();
+  res.render("index", { shortURLs });
+});
+
+app.post("/shorten", async (req, res) => {
+  const fullUrl = req.body.fullUrl;
+  const existing = await ShortURL.findOne({ full: fullUrl });
+  if (existing) return res.redirect("/");
+
+  const short = nanoid(6);
+  await ShortURL.create({ full: fullUrl, short });
+  res.redirect("/");
+});
+
+app.get("/:short", async (req, res) => {
+  const shortUrl = await ShortURL.findOne({ short: req.params.short });
+  if (!shortUrl) return res.status(404).send("Not found");
+
+  shortUrl.clicks++;
+  await shortUrl.save();
+  res.redirect(shortUrl.full);
+});
+
+app.listen(3000, () => console.log("Server started on http://localhost:3000"));
